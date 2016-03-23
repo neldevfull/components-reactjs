82c73bb82313e98bb294d57029c61a38f385870d
neldevfull <nfrugeri@gmail.com>
2016-03-23 19:08:28 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var HelloWorld = React.createClass({
+    render: function(){
+        return (
+            <p>
+                Hello World,
+                <span className='label label-primary'>
+                    {this.props.name}
+                </span>
+            </p>
+        );
+    }
+});
+
+ReactDOM.render(
+    <HelloWorld name="Nelson"/>,
+    document.getElementById('main')
+)
